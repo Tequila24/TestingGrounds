dedcbd01e5237eff4eb9cee77759173d7eb56a5c
Tequila <1>
2020-02-13 16:44:25 +0300

    Wheels revamp

---
+++ Assets/Scripts/WheelControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+public class WheelControl : MonoBehaviour
+{
+    public bool isDrive = false;
+    public bool isSteerable = false;
+    public bool isGrounded = false;
+    public float steerAngle = 0;
+    
+
+    [SerializeField]
+    private float wheelMass = 1;
+    [SerializeField]
+    [Range (0.01f ,0.99f)]
+    private float dampingValue = 0.5f;
+    [SerializeField]
+    private float springValue = 1;
+    [SerializeField]
+    private float StrutToTop = 0;    
+    [SerializeField]
+    private float StrutToBottom = 0;
+    [SerializeField]
+    private float CasterAngle = 0;
+    [SerializeField]
+    private float CamberAngle = 0;
+
+
+    private bool isWheelRight = false;
+}
---
+++ Assets/Scripts/WheelMaster.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+
+public class WheelMaster : MonoBehaviour
+{
+    Rigidbody carBody = null;
+
+    List<WheelControl> wheels = new List<WheelControl>();
+    Vector3 sharedNormal = Vector3.up;
+
+
+
+    void Awake()
+    {
+        carBody = this.gameObject.transform.parent.Find("CarBody").GetComponent<Rigidbody>();
+
+        GetWheels();
+    }
+
+    
+    void GetWheels()
+    {
+        for (int i = 0; i < this.gameObject.transform.childCount; i++)
+        {
+            WheelControl control = this.gameObject.transform.GetChild(i).GetComponent<WheelControl>();
+            wheels.Add(control);
+        }
+    }
+
+
+
+    void FixedUpdate()
+    {
+        foreach (WheelControl wheel in wheels)
+        {
+            UpdateWheelPosition();
+
+            UpdateWheelRotation();
+
+            ApplyCarPhysics();
+        }
+    }
+
+    void UpdateWheelPosition()
+    {
+
+    }
+
+    void UpdateWheelRotation()
+    {
+
+    }
+
+    void ApplyCarPhysics()
+    {
+        UpdateWheelsNormal();
+    }
+
+    void UpdateWheelsNormal()
+    {
+        Vector3 wheelLocalPosition;
+        Vector3 nextWheelLocalPosition;
+        Vector3 cross;
+
+        int i = 0;
+        for (; i < (wheels.Count-1); i++)
+        {
+            wheelLocalPosition = wheels[i].transform.position - this.transform.position;
+            nextWheelLocalPosition = wheels[i+1].transform.position - this.transform.position;
+            cross = Vector3.Cross(wheelLocalPosition, nextWheelLocalPosition);
+            sharedNormal += cross;
+            //Debug.DrawRay(this.transform.position, cross, Color.yellow, Time.deltaTime*25f, false);
+        }
+        wheelLocalPosition = wheels[wheels.Count-1].transform.position - this.transform.position;
+        nextWheelLocalPosition = wheels[0].transform.position - this.transform.position;
+        cross = Vector3.Cross(wheelLocalPosition, nextWheelLocalPosition);
+        sharedNormal += cross;
+        Debug.DrawRay(this.transform.position, wheelLocalPosition, Color.blue, Time.deltaTime*25f, false);
+        Debug.DrawRay(this.transform.position, nextWheelLocalPosition, Color.blue, Time.deltaTime*25f, false);
+        Debug.DrawRay(this.transform.position, cross, Color.red, Time.deltaTime*25f, false);
+
+
+        sharedNormal /= wheels.Count;
+
+        Debug.DrawRay(this.transform.position, sharedNormal, Color.red, Time.deltaTime, false);
+    }
+
+
+
+    void OnDrawGizmos()
+    {
+
+    }
+}
